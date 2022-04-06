2211813e275e9f2e6df57b5f2c5c2526c5ca5643
Karthik Sreenivasan <karthik.sreenivasan.work@gmail.com>
2022-04-06 06:40:27 +0530

    Dividing our view into smaller components and adding the supporting javascript and css files in the angular.json to support it.

--- Frontend/ShoppingCart/src/app/app.module.ts
+++ Frontend/ShoppingCart/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ListproductsComponent } from './Products/listproducts/listproducts.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategoriesComponent,
+    ListproductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
