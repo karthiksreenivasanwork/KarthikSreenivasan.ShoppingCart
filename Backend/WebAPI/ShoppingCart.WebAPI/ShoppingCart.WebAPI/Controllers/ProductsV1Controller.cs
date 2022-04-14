9457e7aa5a6e5528ef4349adda0a4a0d169cd458
Karthik Sreenivasan <karthik.sreenivasan.work@gmail.com>
2022-04-14 20:00:50 +0530

    New Web API solition in .Net Core 5. Added the following controllers with version support. 1. Product 2. Users
    Installed the following NuGet packages.
    1. Microsoft.IdentityModel.Tokens (JWT Support)
    2. System.IdentityModel.Tokens.Jwt
    3. FluentValidation.AspNetCore

---
+++ Backend/WebAPI/ShoppingCart.WebAPI/ShoppingCart.WebAPI/Controllers/ProductsV1Controller.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShoppingCart.WebAPI.Controllers
+{
+    [ApiVersion("1")] //Not applied to controller route for supporting Swagger implementation.
+    [Route("api/v1/Products")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "Products Controller - V1")]
+    public class ProductsV1Controller : ControllerBase
+    {
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1 from version controller 1" };
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Backend/WebAPI/ShoppingCart.WebAPI/ShoppingCart.WebAPI/Startup.cs
@@ -0,0 +1,83 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ShoppingCart Web API", Version = "v1" });
+                /**
+                 * Helps swagger identify the controller based on versioning without conflicts.
+                 * Error Details: Actions require a unique method/path combination for Swagger/OpenAPI 3.0
+                 * Solution: Call ResolveConflictingActions.
+                 */
+                c.ResolveConflictingActions(apiDescription => apiDescription.First());
+
+                /*
+                 * Add custom name to controllers for displaying in the Swagger documentation.
+                 * Group name provided in the ApiExplorerSettings attribute will be mapped.
+                 */
+                c.TagActionsBy(apiDescriptionRef => new[] { apiDescriptionRef.GroupName });
+                c.DocInclusionPredicate((groupName, apiDescriptionRef) => true);
+            });
+
+            /*
+             * Apply versioning for each controller.
+             */
+            services.AddApiVersioning(apiVersioningServiceRef =>
+            {
+                apiVersioningServiceRef.DefaultApiVersion = new ApiVersion(1, 0);
+                apiVersioningServiceRef.AssumeDefaultVersionWhenUnspecified = true;
+                apiVersioningServiceRef.ReportApiVersions = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ShoppingCart.WebAPI v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            //Match controller to each API version of the controller.
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
