1de5e17870ff771ae55c33ecb47a28649cf5733e
Karthik Sreenivasan <karthik.sreenivasan.work@gmail.com>
2022-05-12 07:56:45 +0530

    Completed the following --------------------------
    Angular
    --------
    Added the ability for the user to add the item to the cart from the view product page itself.

--- Frontend/ShoppingCart/src/app/products/viewproduct/viewproduct.component.ts
+++ Frontend/ShoppingCart/src/app/products/viewproduct/viewproduct.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { IProductModel } from '../IProductModel';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
 
 /**
  * Displays unique product information to the user.
@@ -12,6 +13,9 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./viewproduct.component.css'],
 })
 export class ViewproductComponent implements OnInit, OnDestroy {
+  userMessage: string = '';
+  userErrorStatus: boolean = false;
+
   public productModel: IProductModel = {
     productCategoryID: 0,
     productCategoryName: '',
@@ -25,15 +29,44 @@ export class ViewproductComponent implements OnInit, OnDestroy {
 
   public parameterSubscription: Subscription;
 
-  constructor(public activeRoute: ActivatedRoute) {}
+  constructor(
+    public activeRoute: ActivatedRoute,
+    public cartService: CartService
+  ) {}
 
   ngOnInit(): void {
-    this.parameterSubscription = this.activeRoute.queryParams.subscribe(params => {
-      this.productModel = JSON.parse(params["productinfo"]);
-   });
+    this.parameterSubscription = this.activeRoute.queryParams.subscribe(
+      (params) => {
+        this.productModel = JSON.parse(params['productinfo']);
+      }
+    );
   }
 
   ngOnDestroy(): void {
     this.parameterSubscription.unsubscribe();
+  }
+
+  /**
+   * Event handler when an 'Add to Cart' button is clicked for a product.
+   * @param productID Product ID to be added.
+   */
+  onAddToCartClick(productID: number) {
+    this.userMessage = '';
+    this.userErrorStatus = false;
+
+    this.cartService.addItemsToCart(productID.toString()).subscribe({
+      next: (data: any) => {
+        if (data != null) {
+          this.userMessage = `Product '${data.productname}' added to cart`;
+        } else this.userMessage = 'Product added to cart';
+        //Only update the cart count when the cart item has been successfully saved to the database.
+        this.cartService.updateCartCountSubj.next('');
+      },
+      error: (error) => {
+        this.userErrorStatus = true;
+        this.userMessage = 'Something went wrong!';
+        console.log(error);
+      },
+    });
   }
 }
